30ed4aad70d769e6fee306e3258234dac298da70
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-05 03:23:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,12 +4,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
+  brands = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-    BRANDS = BRANDS.uniq
+    brands << brand
+    brands = BRANDS.uniq
   end
 
 
